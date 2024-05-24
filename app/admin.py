6500b14f298ab85ba1f328686b5d99f37a5a9ac4
cs_lee <47213853+2044smile@users.noreply.github.com>
2024-05-24 18:52:28 +0900

    ✨ Feat: Add a admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Hall
+
+
+@admin.register(Hall)
+class HallAdmin(admin.ModelAdmin):
+    list_display = ['teacher', 'student_list', 'attend_code', 'is_closed']
+
+    def student_list(self, obj):
+        return ",\n".join([i.user_id for i in obj.students.all()])
