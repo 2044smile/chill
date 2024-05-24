bfc99dab998eeba745db1a59fb1405167dc61b59
cs_lee <47213853+2044smile@users.noreply.github.com>
2024-05-24 18:54:56 +0900

    ✨ Feat: Add a field on User, Hall Model and I use the save() method

--- account/models.py
+++ account/models.py
@@ -1,3 +1,81 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, user_id, password, **extra_fields):
+        if not user_id:
+            raise ValueError('must have an user_id')
+        if not password:
+            raise ValueError('must have a password')
+        
+        user = self.model(
+            user_id=user_id,
+            password=password,
+            **extra_fields
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        
+        return user
+    
+    def create_superuser(self, user_id, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        
+        return self.create_user(user_id, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    objects = UserManager()
+
+    STUDENT = 'student'
+    TEACHER = 'teacher'
+    ADMIN = 'admin'
+
+    STATUS_CHOICE = (
+        (STUDENT, '수강생'),
+        (TEACHER, '강사'),
+        (ADMIN, '관리자')
+    )
+    user_id = models.CharField(
+        verbose_name='user_id',
+        max_length=32,
+        unique=True
+    )
+    password = models.CharField(
+        verbose_name='password',
+        max_length=255,
+    )
+    status = models.CharField(
+        verbose_name='회원 정보',
+        max_length=16,
+        choices=STATUS_CHOICE, 
+    )
+    x_user_id = models.CharField(
+        verbose_name='회원 ID',
+        max_length=16
+    )
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'user_id'
+
+    def __str__(self):
+        return self.user_id
+        
+    def save(self, *args, **kwargs):
+        if self.x_user_id in ['0', 0, '']:
+            import random, string
+            target = string.ascii_letters + string.digits
+            self.x_user_id = "".join(random.choices(target, k=10))
+
+            super().save(*args, **kwargs)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+from account.models import User
+from config.exceptions import OvercapacityOfStudents
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="생성 일시",
+        help_text="데이터가 생성 된 날짜 입니다."
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name="수정 일시",
+        help_text="데이터가 수정 된 날짜 입니다."
+    )
+
+    class Meta:
+        abstract = True
+
+
+class Hall(BaseModel):
+    teacher = models.OneToOneField(User, on_delete=models.CASCADE, related_name='teacher', limit_choices_to={'status': User.TEACHER})
+    students = models.ManyToManyField(User, related_name='students', limit_choices_to={'status': User.STUDENT}, blank=True)
+    available = models.IntegerField(default=0, verbose_name='최대 참석 가능 수강생 인원')
+    attend_code = models.CharField(max_length=8, verbose_name='강의장 참석을 위한 코드', default=0)
+    x_room_id = models.CharField(max_length=16, verbose_name='강의장 ID', default=0)
+
+    is_closed = models.BooleanField(default=True, verbose_name="강의장 종료")
+        
+    def save(self, *args, **kwargs):
+        if self.attend_code in ['0', 0]:
+            import random, string
+            target = string.ascii_letters + string.digits
+            self.attend_code = "".join(random.choices(target, k=5))  # 고유값 무작위 문자열
+            self.x_room_id = "".join(random.choices(target, k=10))
+
+            super().save(*args, **kwargs)
